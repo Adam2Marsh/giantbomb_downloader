dfef7c4f5a981cc3e291cddb43a2123cafb24478
Adam Marsh <adam.marsh@rocketmail.com>
2016-01-13 19:53:33 +0000

    Centered Header Text

--- resources/views/parts/head.blade.php
+++ resources/views/parts/head.blade.php
@@ -2,7 +2,7 @@
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 	
 	<div class="page-header">
-		<h1>GB_Downloads</h1>
+		<h1 class="text-center">GB_Downloads</h1>
 	</div>
 
 </head>
