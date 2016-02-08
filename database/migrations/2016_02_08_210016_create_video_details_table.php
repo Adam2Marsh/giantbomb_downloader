2f86a94bfa7c58eeb0e1be5f78eb103bb9668fe6
Adam Marsh <adam.marsh@rocketmail.com>
2016-02-08 21:04:13 +0000

    Initial Commit for Video Details Table

---
+++ database/migrations/2016_02_08_210016_create_video_details_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideoDetailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video_details', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('local_path');
+            $table->string('image_path');
+            $table->bigInteger('file_size');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('video_details');
+    }
+}
