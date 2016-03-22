3c4d8abdaf6c7ecb1bccfc88edf94208bc65182c
Adam Marsh <adam.marsh@rocketmail.com>
2016-03-22 19:09:16 +0000

    Videos Table seeded in loop correctly

--- database/seeds/FillVideoTable.php
+++ database/seeds/FillVideoTable.php
@@ -12,26 +12,9 @@ class FillVideoTable extends Seeder
      */
     public function run()
     {
-        $this->CreateVideo("Kerbal_Project_B.E.A.S.T_-_Part_1", 1, "test.com", '2016-01-01');
-        $this->CreateVideo("Kerbal_Project_B.E.A.S.T_-_Part_1", 2, "test.com", '2016-01-01');
-        $this->CreateVideo("Kerbal_Project_B.E.A.S.T_-_Part_1", 3, "test.com", '2016-01-01');
-        $this->CreateVideo("Kerbal_Project_B.E.A.S.T_-_Part_1", 4, "test.com", '2016-01-01');
-        $this->CreateVideo("Kerbal_Project_B.E.A.S.T_-_Part_1", 5, "test.com", '2016-01-01');
-        $this->CreateVideo("Kerbal_Project_B.E.A.S.T_-_Part_1", 6, "test.com", '2016-01-01');
-        $this->CreateVideo("Kerbal_Project_B.E.A.S.T_-_Part_1", 7, "test.com", '2016-01-01');
-        $this->CreateVideo("Kerbal_Project_B.E.A.S.T_-_Part_1", 8, "test.com", '2016-01-01');
-        $this->CreateVideo("Kerbal_Project_B.E.A.S.T_-_Part_1", 9, "test.com", '2016-01-01');
-        $this->CreateVideo("Kerbal_Project_B.E.A.S.T_-_Part_1", 10, "test.com", '2016-01-01');
-        $this->CreateVideo("Kerbal_Project_B.E.A.S.T_-_Part_1", 11, "test.com", '2016-01-01');
-        $this->CreateVideo("Kerbal_Project_B.E.A.S.T_-_Part_1", 12, "test.com", '2016-01-01');
-        $this->CreateVideo("Kerbal_Project_B.E.A.S.T_-_Part_1", 13, "test.com", '2016-01-01');
-        $this->CreateVideo("Kerbal_Project_B.E.A.S.T_-_Part_1", 14, "test.com", '2016-01-01');
-        $this->CreateVideo("Kerbal_Project_B.E.A.S.T_-_Part_1", 15, "test.com", '2016-01-01');
-        $this->CreateVideo("Kerbal_Project_B.E.A.S.T_-_Part_1", 16, "test.com", '2016-01-01');
-        $this->CreateVideo("Kerbal_Project_B.E.A.S.T_-_Part_1", 17, "test.com", '2016-01-01');
-        $this->CreateVideo("Kerbal_Project_B.E.A.S.T_-_Part_1", 18, "test.com", '2016-01-01');
-        $this->CreateVideo("Kerbal_Project_B.E.A.S.T_-_Part_1", 19, "test.com", '2016-01-01');
-        $this->CreateVideo("Kerbal_Project_B.E.A.S.T_-_Part_1", 20, "test.com", '2016-01-01');
+    	for ($i=0; $i < 50; $i++) { 
+    		$this->CreateVideo("Kerbal_Project_B.E.A.S.T_-_Part_$i", $i, "test.com", '2016-01-01');
+    	}
     }
 
 
