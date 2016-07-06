e37235f0518ec9c08deb5244a84d127b3bf8114b
Adam Marsh <adam.marsh@rocketmail.com>
2016-07-06 23:35:12 +0100

    Changes to App Config

--- config/gb.php
+++ config/gb.php
@@ -4,7 +4,7 @@
 		'api_key' => '71db34446b406a050e61a23b28fdcdfbb22917f1',
 		'max_downloads_per_day' => 5,
 		'Website_Address' => 'http://www.giantbomb.com/api/videos/',
-		'Latest_Video_Query' => '?api_key=KEY_HERE&format=json&subscriber_only=true&limit=10&field_list=id,name,hd_url,publish_date',
+		'Latest_Video_Query' => '?api_key=KEY_HERE&format=json&subscriber_only=true&limit=30&field_list=id,name,hd_url,publish_date',
 		
 		'index_show_days_video' => env('INDEX_SHOW_DAYS_VIDEO',30),
 	];
