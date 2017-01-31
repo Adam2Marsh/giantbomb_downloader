006db71fa780db0d4fb438113e2a50b4d394cc66
Adam Marsh <adam.marsh@rocketmail.com>
2017-01-31 08:08:04 +0000

    Removed some bolier plate code which wasn't being used

--- app/Http/Controllers/ServiceCaller.php
+++ app/Http/Controllers/ServiceCaller.php
@@ -10,25 +10,18 @@ use Log;
 
 class ServiceCaller extends Controller
 {
-    
+
 	public function newVideos()
 	{
 		$dvi = new \App\Services\DownloadVideoInformation;
 
 		Log::info(__METHOD__." Controller has been called to retireve all new videos and add into database");
-		
-		// $dvi->UpdateVideosInDatabase(config('gb.Website_Address'), config('gb.Latest_Video_Query'), config('gb.api_key'));
 
 		$url=env('TEST_JSON_URL',config('gb.Website_Address'));
 		$query=env('LATEST_VIDEO_QUERY',config('gb.Latest_Video_Query'));
 		$apiKey=env('GB_API_KEY',config('gb.api_key'));
 
 		$dvi->UpdateVideosInDatabase($url,$query,$apiKey);
-	}
-
-	public function ScheduleVideos()
-	{
-		return 'Bye Bye';
 	}
 
 }
