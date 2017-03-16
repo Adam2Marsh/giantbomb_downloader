67c30dec9e8372e5f69bc7230dc51caadc8fae0e
Adam Marsh <adam.marsh@rocketmail.com>
2017-03-16 19:47:14 +0000

    Changed default configs and added db creation into playbook

--- config/gb.php
+++ config/gb.php
@@ -3,8 +3,8 @@
     return [
         'max_downloads_per_day' => 5,
         'Website_Address' => 'https://www.giantbomb.com/api/videos/',
-        'Latest_Video_Query' => '?api_key=KEY_HERE&format=json&subscriber_only=true&limit=30&field_list=id,name,hd_url,high_url,low_url,publish_date,deck,image',
+        'Latest_Video_Query' => '?api_key=KEY_HERE&format=json&subscriber_only=true&limit=20&field_list=id,name,hd_url,high_url,low_url,publish_date,deck,image',
         'index_show_days_video' => env('INDEX_SHOW_DAYS_VIDEO', 30),
         'storage_limit' => env('STORAGE_LIMIT', 20000000000),
-        'use_wget_to_download' => env('USE_WGET', false),
+        'use_wget_to_download' => env('USE_WGET', true),
     ];
