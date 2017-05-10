2975ff42b51141b2b4284556cf732cbe87a66515
Adam Marsh <adam.marsh@rocketmail.com>
2017-05-10 07:19:38 +0100

    Changed from php exec to Symfony Process

--- app/Services/UpdateService.php
+++ app/Services/UpdateService.php
@@ -3,19 +3,28 @@
 namespace App\Services;
 
 use Log;
+use Symfony\Component\Process\Process;
 
 class UpdateService
 {
 
     public function isThereAUpdate()
     {
-        Log::info(__METHOD__ . "Checking for updates");
+        Log::info(__METHOD__ . " - Checking for updates");
 
-        $output = exec('cd /opt/giantbomb_downloader | git fetch && git status');
+        $process = new Process('cd /opt/giantbomb_downloader | git fetch && git status');
 
-        Log::info(__METHOD__ . "Raw Output from check is: " . $output);
+        $process->run();
 
-        if (str_contains($output, 'Your branch is up-to-date')) {
+        if ($process->isSuccessful()) {
+            Log::info(__METHOD__ . " - Raw Output from check is: " . $process->getOutput());
+
+            if (str_contains($process->getOutput(), 'Your branch is up-to-date')) {
+                return false;
+            }
+        } else {
+            Log::info(__METHOD__ . " - Something failed when checking for update " . $process->getErrorOutput());
+
             return false;
         }
 
@@ -24,8 +33,12 @@ class UpdateService
 
     public function update()
     {
-        Log::info(__METHOD__ . "I've been asked to update, let's do this");
+        Log::info(__METHOD__ . - "I've been asked to update, let's do this");
 
-        exec('cd /opt/giantbomb_downloader | git pull');
+//        exec('cd /opt/giantbomb_downloader | git pull');
+
+        $process = new Process('cd /opt/giantbomb_downloader | git pull');
+
+        $process->run();
     }
 }
