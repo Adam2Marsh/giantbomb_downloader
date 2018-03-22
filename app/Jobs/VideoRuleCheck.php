e3b2351da714837c4eace2d529ce50d63bb64528
Adam Marsh <adam.marsh@rocketmail.com>
2018-03-22 21:25:31 +0000

    Addedd Rule check to auto download videos

--- app/Jobs/VideoRuleCheck.php
+++ app/Jobs/VideoRuleCheck.php
@@ -8,6 +8,10 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
+use App\Rule;
+use App\Repositories\VideoRepository;
+use Log;
+
 class VideoRuleCheck implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
@@ -29,8 +33,16 @@ class VideoRuleCheck implements ShouldQueue
      *
      * @return void
      */
-    public function handle()
+    public function handle(VideoRepository $videoRepository)
     {
-        //
+        Log::info("Checking if " . $this->video->name . "matches any rules");
+        foreach (Rule::all() as $rule) {
+            if($rule->enabled) {
+                if(preg_match("/".$rule->rule."/", $this->video->name)) {
+                    Log::info($this->video->name . " matched rule " . $rule->rule . " so adding to download queue");
+                    $videoRepository->updateVideoState($this->video->id, "queued");
+                }
+            }
+        }
     }
 }
